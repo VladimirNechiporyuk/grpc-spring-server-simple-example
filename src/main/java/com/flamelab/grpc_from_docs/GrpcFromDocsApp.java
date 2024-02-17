d6256bb49de10ecb77c05891886d5440540a03fe
volodymyrnechiporyuk <vnechiporyuk@casgate.io>
2024-02-17 09:55:41 +0200

    Initial commit

---
+++ src/main/java/com/flamelab/grpc_from_docs/GrpcFromDocsApp.java
@@ -0,0 +1,11 @@
+package com.flamelab.grpc_from_docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcFromDocsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcFromDocsApp.class, args);
+    }
+}
